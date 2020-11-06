ffa1367d389e2dc901a19c9b2f090e1e7d6225c0
EC2 Default User <ec2-user@ip-172-31-14-244.ec2.internal>
2020-11-06 21:12:11 +0000

    Controle de Categoria - eSocial

---
+++ K88Uk0XAoEIXHcmPayrollEnumQuery/index.js
@@ -0,0 +1,41 @@
+
+/**
+ * Nome da primitiva : enumQuery
+ * Nome do dominio : hcm
+ * Nome do serviço : payroll
+ * Nome do tenant : trn94657786
+ **/
+
+exports.handler = async (event) => {
+    let body = parseBody(event);
+
+   //Retira valor 999 da lista de categoria eSocial
+   
+   if(body.results) {
+      if(body.results[0].enumName === 'ESocialCategory') {
+      
+        body.results[0].items = body.results[0].items.filter(element => {
+           return element.key !== 'Category_999';
+        });
+      }
+   }
+
+  return sendRes(200,body);
+    
+
+};
+
+const parseBody = (event) => {
+    return typeof event.body === 'string' ?  JSON.parse(event.body) : event.body || {};
+};
+
+const sendRes = (status, body) => {
+    var response = {
+      statusCode: status,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: typeof body === 'string' ? body : JSON.stringify(body) 
+    };
+    return response;
+};
